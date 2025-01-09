bfa6fbe6c990c5216d9d3c6902e26fa28092755c
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-09 12:27:14 +0000

    04
    exercise 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let new_array = array.filter((element) => !remove.includes(element));
+    return new_array;
 };
 
 // Do not edit below this line
