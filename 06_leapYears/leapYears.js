dbeb792fae21b1b0e021ec528802d04b451c88fe
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-09 12:41:34 +0000

    06
    exercise 06 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let result
+
+    if (year % 400 == 0) {
+        result = true;
+    }   else if (year % 100 == 0) {
+        result = false
+    }   else if (year % 4 == 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+
+    return result;
 
 };
 
