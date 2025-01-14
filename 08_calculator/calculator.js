9329d2781430ad886c1b6450ec87f8e79c8362c5
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-14 10:50:10 +0000

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(n_1, n_2) {
+  res = n_1 + n_2;
+  return res;
 	
 };
 
-const subtract = function() {
+const subtract = function(n_1, n_2) {
+  res = n_1 - n_2;
+  return res;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let res = 0;
+  const result = arr.reduce((total, curr) => total + curr, 0);
+
+  return result;
+};
+
+const multiply = function(arr) {
+  let result = arr.reduce((total, current) => total * current, 1);
+
+  return result;
+
+};
+
+const power = function(n, pow) {
+  let result = Math.pow(n, pow);
+
+  return result;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 	
 };
 
