e4ae3a7cd3f4196f59ef51ea36415bf7db2ad884
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-14 11:20:11 +0000

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+
+    let arr_birth = obj.map((x) => x.yearOfBirth);
+
+    let arr_death = obj.map((x) => x.yearOfDeath);
+
+    let ages = [];
+
+    arr_birth.map((x, i) => {
+        if (arr_death[i] === undefined) {
+            ages.push(new Date().getFullYear() - x);
+        } else {
+            ages.push(arr_death[i] - x);
+        }
+    });
+
+    let maxIdx = ages.indexOf(Math.max(...ages));
+
+    return obj[maxIdx];
+
 
 };
 
